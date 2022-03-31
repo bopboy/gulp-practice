a2515f9584510e6edaa83e349c755f26e102f540
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-01 05:16:03 +0900

    10 scss 태스크 추가
    layout.pug 에 css 읽어오는 부분 안넣고 한찬 헤맴

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,12 @@ import gpug from 'gulp-pug'
 import del from 'del'
 import ws from 'gulp-webserver'
 import image from 'gulp-image'
+import sass from 'gulp-sass'
+sass.compiler = require('node-sass')
+// const sass = require('gulp-sass')(require('sass'));
+// import dartSass from 'sass';
+// import gulpSass from 'gulp-sass';
+// const sass = gulpSass(dartSass);
 
 const routes = {
     pug: {
@@ -13,26 +19,34 @@ const routes = {
     img: {
         src: "src/img/*",
         dest: "build/img"
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css/"
     }
 }
 const pug = () =>
-    gulp
-        .src(routes.pug.src)
+    gulp.src(routes.pug.src)
         .pipe(gpug())
         .pipe(gulp.dest(routes.pug.dest))
 const img = () =>
-    gulp
-        .src(routes.img.src)
+    gulp.src(routes.img.src)
         .pipe(image())
         .pipe(gulp.dest(routes.img.dest))
-
+const styles = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(routes.scss.dest))
 const clean = () => del(["build"])
 const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }))
 const watch = () => {
     gulp.watch(routes.pug.watch, pug)
     gulp.watch(routes.img.src, img)
+    gulp.watch(routes.scss.watch, styles)
 }
 const prepare = gulp.series([clean, img])
-const assets = gulp.series([pug])
-const postDev = gulp.parallel([webserver, watch])
-export const dev = gulp.series([prepare, assets, postDev])
+const assets = gulp.series([pug, styles])
+const live = gulp.parallel([webserver, watch])
+export const dev = gulp.series([prepare, assets, live])
