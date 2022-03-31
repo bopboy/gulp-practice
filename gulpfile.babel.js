d2658320e3668a69fb7bbed076d3f3a6e80291cc
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-01 05:48:26 +0900

    12 js 태스크

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,8 @@ import image from 'gulp-image'
 import sass from 'gulp-sass'
 import autoprefixer from 'gulp-autoprefixer'
 import miniCSS from 'gulp-csso'
+import bro from 'gulp-bro'
+import babelify from 'babelify'
 
 sass.compiler = require('node-sass')
 // const sass = require('gulp-sass')(require('sass'));
@@ -27,6 +29,11 @@ const routes = {
         watch: "src/scss/**/*.scss",
         src: "src/scss/style.scss",
         dest: "build/css/"
+    },
+    js: {
+        watch: "src/js/**/*.js",
+        src: "src/js/main.js",
+        dest: "build/js"
     }
 }
 const pug = () =>
@@ -38,20 +45,30 @@ const img = () =>
         .pipe(image())
         .pipe(gulp.dest(routes.img.dest))
 const styles = () =>
-    gulp
-        .src(routes.scss.src)
+    gulp.src(routes.scss.src)
         .pipe(sass().on("error", sass.logError))
         .pipe(autoprefixer({ browsers: ["last 2 versions"] }))
         .pipe(miniCSS())
         .pipe(gulp.dest(routes.scss.dest))
+const js = () =>
+    gulp.src(routes.js.src)
+        .pipe(bro({
+            transform: [
+                babelify.configure({ presets: ["@babel/preset-env"] }),
+                ["uglifyify", { global: true }]
+            ]
+        }))
+        .pipe(gulp.dest(routes.js.dest))
+
 const clean = () => del(["build"])
 const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }))
 const watch = () => {
     gulp.watch(routes.pug.watch, pug)
     gulp.watch(routes.img.src, img)
     gulp.watch(routes.scss.watch, styles)
+    gulp.watch(routes.js.watch, js)
 }
 const prepare = gulp.series([clean, img])
-const assets = gulp.series([pug, styles])
+const assets = gulp.series([pug, styles, js])
 const live = gulp.parallel([webserver, watch])
 export const dev = gulp.series([prepare, assets, live])
